a34f1342e3f22c1c354bb261949e5fcf4b129bfe
GraceOyiza <graceoyiza13@gmail.com>
2021-05-15 22:52:23 +0100

    Generate and add api key to index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import './scss/style.scss';
+let weather = {
+  apiKey: 'c20d664fe15511ff6b176f9e20df512f',
+  fetchweather: function (city) {
+    fetch(
+      'http://api.openweathermap.org/data/2.5/weather?q= ' 
+    )
+     
+};
