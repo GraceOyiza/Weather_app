cbd6407a579199e3bfa39b05073b50bcf38ff8ea
GraceOyiza <graceoyiza13@gmail.com>
2021-05-16 14:53:22 +0100

    Fetching data and apiKey

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import './scss/style.scss';
+
 let weather = {
   apiKey: 'c20d664fe15511ff6b176f9e20df512f',
   fetchweather: function (city) {
     fetch(
-      'http://api.openweathermap.org/data/2.5/weather?q= ' 
+      'http://api.openweathermap.org/data/2.5/weather?q= ' +
+        city +
+        '&units=metric&appid=' +
+        this.apiKey,
     )
-     
+      .then((response) => response.json())
+      .then((data) => this.displayweather(data));
+  },
 };
