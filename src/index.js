09f9744732f36c4aaacd23ab6eb4455e0ee3c446
GraceOyiza <graceoyiza13@gmail.com>
2021-05-16 15:03:48 +0100

    Add displayweather function

--- src/index.js
+++ src/index.js
@@ -12,4 +12,24 @@ let weather = {
       .then((response) => response.json())
       .then((data) => this.displayweather(data));
   },
+  displayweather: function (data) {
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+    console.log(name, icon, description, temp, humidity, speed);
+
+    document.querySelector('.city').innerText = 'Weather in ' + name;
+    document.querySelector('.icon').src =
+      'https://openweathermap.org/img/wn/"+ icon +" 01n@.png';
+    document.querySelector('.description').innerText = description;
+    document.querySelector('.temp').innerText = temp + '°C';
+    document.querySelector('.humidity').innerText =
+      'Humidity: ' + humidity + '%  ';
+    document.querySelector('.wind').innerText =
+      'Wind speed ' + speed + ' km/h  ';
+    document.querySelector('.weather').classList.remove('Loading');
+    document.body.style.backgroundImage =
+      "url('https://source.unsplash.com/1600x900/?" + name + " ')";
+  },
 };
