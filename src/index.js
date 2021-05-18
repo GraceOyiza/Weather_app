c8c8c68303f7d2327b349ac45504735939da70bf
GraceOyiza <graceoyiza13@gmail.com>
2021-05-18 14:41:59 +0100

    Refactor

--- src/index.js
+++ src/index.js
@@ -1,64 +1,37 @@
 import './scss/style.scss';
-import searchIcon from './images/search-icon.png';
 
-let weather = {
-  apiKey: 'c20d664fe15511ff6b176f9e20df512f',
-  fetchweather: (city) => {
-    fetch(
-      `http://api.openweathermap.org/data/2.5/weather?q= ${city}&APPID=${this.apiKey}&units=${unit}`,
-    )
-      .then((response) => response.json())
-      .then((data) => this.displayweather(data));
-  },
-  displayweather: (data) => {
-    const { name } = data;
-    const { description } = data.weather[0];
-    const { temp, humidity } = data.main;
-    const { speed } = data.wind;
+const weatherForm = document.getElementById('weather-form');
+const apiKey = 'c20d664fe15511ff6b176f9e20df512f';
 
-    document.querySelector('.city').innerText = 'Weather in ' + name;
-    document.querySelector('.icon').src =
-      'https://openweathermap.org/img/wn/"+ icon +" 01n@.png';
-    document.querySelector('.description').innerText = description;
-    document.querySelector('.temp').innerText = temp + '°C';
-    document.querySelector('.humidity').innerText =
-      'Humidity: ' + humidity + '%  ';
-    document.querySelector('.wind').innerText =
-      'Wind speed ' + speed + ' km/h  ';
-    document.querySelector('.weather').classList.remove('Loading');
-    document.body.style.backgroundImage =
-      "url('https://source.unsplash.com/1600x900/?" + name + " ')";
-  },
-  search: () => {
-    this.fetchweather(document.querySelector('.search-bar').value);
-  },
-};
+weatherForm.addEventListener('submit', (event) => {
+  event.preventDefault();
 
-document.querySelector('.search button').addEventListener('click', function () {
-  weather.search();
+  const formData = new FormData(weatherForm);
+  const location = formData.get('location');
+  const unit = formData.get('unit');
+  const endpoint = `http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${apiKey}&units=${unit}`;
+
+  fetch(endpoint)
+    .then((response) => response.json())
+    .then((data) => {
+      const { name } = data;
+      const { icon, description } = data.weather[0];
+      const { temp, humidity } = data.main;
+      const { speed } = data.wind;
+      const unitSymbol = unit === 'metric' ? '°C' : '°F';
+      const windSpeed = unit === 'metric' ? 'km/h' : 'm/s';
+
+      document.querySelector('.city').innerText = 'Weather in ' + name;
+      document.querySelector('.icon').src =
+        'https://openweathermap.org/img/wn/"+ icon +" 01n@.png';
+      document.querySelector('.description').innerText = description;
+      document.querySelector('.temp').innerText = temp + unitSymbol;
+      document.querySelector('.humidity').innerText =
+        'Humidity: ' + humidity + '%  ';
+      document.querySelector('.wind').innerText =
+        'Wind speed ' + speed + windSpeed;
+      document.querySelector('.weather').classList.remove('Loading');
+      document.body.style.backgroundImage =
+        "url('https://source.unsplash.com/1600x900/?" + name + " ')";
+    });
 });
-
-document
-  .querySelector('.search-bar')
-  .addEventListener('keyup', function (event) {
-    if (event.key == 'Enter') {
-      weather.search();
-    }
-  });
-
-const btnImg = document.querySelector('#btn-img');
-btnImg.src = searchIcon;
-
-// const celciusTofarenheit = (F = (temp * 9.0) / 5 + 32);
-
-// var checkBox = document.getElementById('myCheck');
-// // Get the output text
-// checkBox.addEventListener('click', () => {
-//   var text = document.getElementById('text');
-
-//   if (checkBox.checked == true) {
-//     text.style.display = 'block';
-//   } else {
-//     text.style.display = 'none';
-//   }
-// });
